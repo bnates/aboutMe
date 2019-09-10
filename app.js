937d985c9f74a2ee9f862dd7a713897eb4c48c09
Rosalyn Johnson <rosraejohnson@gmail.com>
2019-09-10 15:41:07 -0700

    added java

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 'use strict'
+
+var name = prompt('What is your name?')
+alert('Hello ' + name + ' welcome to my about me page!')
+var blue = prompt('do you like blue?');
+
+blue.toLowerCase()
+
+if(blue = 'yes'){
+    alert('Me too!')
+}
+else if(blue = 'y'){
+    alert('Good choice')
+}
+else
+    {alert('You should like blue')
+};
+
+var cat = prompt('Do you have a cat?')
+cat.toLowerCase()
+
+if(cat = 'yes'){
+    alert('Cats are great')
+}
+else if(cat = 'y'){
+    alert('you should get a second cat')
+}
+else{
+    alert 
+};
