1b0e2d144b02443d22bbe2dfdd72f6e7518e78e4
Rosalyn Johnson <rosraejohnson@gmail.com>
2019-09-10 16:41:13 -0700

    css reset

--- js/app.js
+++ js/app.js
@@ -6,59 +6,59 @@ var blue = prompt('do you like blue?');
 
 blue.toLowerCase()
 
-if(blue = 'yes'){
-    alert('Me too!')
-}
-else if(blue = 'y'){
-    alert('Good choice')
-}
-else
-    {alert('You should like blue')
-};
+    if(blue = 'yes'){
+        alert('Me too!')
+    }
+    else if(blue = 'y'){
+        alert('Good choice')
+    }
+    else
+        {alert('You should like blue')
+    };
 
-var cat = prompt('Do you have a cat?')
-cat.toLowerCase()
+    var cat = prompt('Do you have a cat?')
+    cat.toLowerCase()
 
-if(cat = 'yes'){
-    alert('Cats are great')
-}
-else if(cat = 'y'){
-    alert('you should get a second cat')
-}
-else{
-    alert 
-};
+    if(cat = 'yes'){
+        alert('Cats are great')
+    }
+    else if(cat = 'y'){
+        alert('you should get a second cat')
+    }
+    else{
+        alert 
+    };
 
-var fun = prompt('Do you like to have fun?')
-fun.toLowerCase()
+    var fun = prompt('Do you like to have fun?')
+    fun.toLowerCase()
 
-if(fun = 'yes'){
-    alert('I like to have fun too!')
-}else if(fun='y'){
-    alert('Fun is good for your mental health')
-} else(){
-    alert('Fun is important; why dont you like to have fun?!')
-};
+    if(fun = 'yes'){
+        alert('I like to have fun too!')
+    }else if(fun='y'){
+        alert('Fun is good for your mental health')
+    } else{
+        alert('Fun is important; why dont you like to have fun?!')
+    };
 
-var sleep = prompt('Do you enjoy sleeping?')
-sleep.toLowerCase()
+    var sleep = prompt('Do you enjoy sleeping?')
+    sleep.toLowerCase()
 
-if(sleep = 'yes'){
-    alert('Sleep is relaxing')
-} else if(sleep = 'y'){
-    alert('You need eight hours of sleep')
-} else(){
-    alert('Are you an ant? Cause they dont sleep')
-};
+    if(sleep = 'yes'){
+        alert('Sleep is relaxing')
+    } else if(sleep = 'y'){
+        alert('You need eight hours of sleep')
+    } else{
+        alert('Are you an ant? Cause they dont sleep')
+    };
 
-var bored = prompt('Are you bored right now?')
-bored.toLowerCase()
-if(bored = 'yes'){
-    alert('Me too')
-} else if(bored = 'y'){
-    alert('sammmmmeeeee')
-}else(){
-    alert('That is nice, jealous')
-};
+    var bored = prompt('Are you bored right now?')
+    bored.toLowerCase()
+    if(bored = 'yes'){
+        alert('Me too')
+    } else if(bored = 'y'){
+        alert('sammmmmeeeee')
+    }else{
+        alert('That is nice, jealous')
+    };
 
 
