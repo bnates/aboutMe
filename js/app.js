834943760ad497ba73480f8e332b65afbf88fa01
Rosalyn Johnson <rosraejohnson@gmail.com>
2019-09-11 13:53:39 -0700

    got java to work

--- js/app.js
+++ js/app.js
@@ -1,69 +1,62 @@
 'use strict';
 
-var name = prompt('What is your name?');
-alert('Hello ' + name + ' welcome to my about me page!');
-var blue = prompt('do you like blue?');
+var username = prompt('What is your name?');
+alert('Hello ' + username + ' welcome to my about me page! First lets do a quick test to see how well you know me.');
+console.log('uesername has entered test');
 
-blue.toLowerCase()
+var blue = prompt('Do I like blue?');
 
-    if(blue = 'yes'){
-        alert('Me too!')
-        console.log('Green is also cool')
-    }
-    else if(blue = 'y'){
-        alert('Good choice')
-        console.log('Pink is also cool')
-    }
-    else
-        {alert('You should like blue')
-    };
-    
+blue = blue.toLowerCase();
 
-    var cat = prompt('Do you have a cat?')
-    cat.toLowerCase()
+if(blue === 'yes' || blue === 'y'){
+  alert('Indeed I do, I aslo like pink and green');
+  console.log('answer was correct');
+} else
+{alert('Oops! I love blue, good try');
+  console.log('answer was incorrect');
+}
 
-    if(cat = 'yes'){
-        alert('Cats are great')
-        console.log('I like dogs too')
-    }
-    else if(cat = 'y'){
-        alert('you should get a second cat')
-        console.log('Someday I want a dog too')
-    }
-    else{
-        alert 
-    };
 
-    var fun = prompt('Do you like to have fun?')
-    fun.toLowerCase()
+var cat = prompt('Do I have a cat?');
+cat = cat.toLowerCase();
 
-    if(fun = 'yes'){
-        alert('I like to have fun too!')
-    }else if(fun='y'){
-        alert('Fun is good for your mental health')
-    } else{
-        alert('Fun is important; why dont you like to have fun?!')
-    };
+if(cat === 'yes' || cat === 'y'){
+  alert('Cats are great and right now I only have one');
+  console.log('correct answer for cat question');
+}else{
+  alert('Turns out I have a cat. She is a  cutie and I spoil her');
+}
 
-    var sleep = prompt('Do you enjoy sleeping?')
-    sleep.toLowerCase()
+var fun = prompt('Do I only play one video game?');
+fun = fun.toLowerCase();
 
-    if(sleep = 'yes'){
-        alert('Sleep is relaxing')
-    } else if(sleep = 'y'){
-        alert('You need eight hours of sleep')
-    } else{
-        alert('Are you an ant? Cause they dont sleep')
-    };
+if(fun === 'yes' || fun==='y'){
+  alert('That is right! I only ever play don/t starve with the DLC shipwrecked');
+  console.log('One game to rule them all');
+}else{
+  alert('It is just the one game actually. Cause I am weird like that.');
+  console.log('They thought I was deverse with my game play');
+}
 
-    var bored = prompt('Are you bored right now?')
-    bored.toLowerCase()
-    if(bored = 'yes'){
-        alert('Me too')
-    } else if(bored = 'y'){
-        alert('sammmmmeeeee')
-    }else{
-        alert('That is nice, jealous')
-    };
+var sleep = prompt('Do I love sleeping in and nap taking?');
+sleep = sleep.toLowerCase();
+
+if(sleep === 'yes' || sleep === 'y'){
+  alert('Sleep is the best and I LOVE it');
+  console.log('sleep is bae');
+}else{
+  alert('Oops! Thank you for thinking that I am not a human sloth, but unfortunatly I am');
+  console.log('I am sloth');
+}
+
+var food = prompt('Do I like to bake and cook?');
+food = food.toLowerCase();
+if(food === 'yes' || food === 'y'){
+  alert('Me tooI have been cooking and baking since I was three!');
+  console.log('I think ' + username + ' wants me to feed them now');
+}else{
+  alert('Nope. I do cook and bake. It relaxes me.');
+  console.log('I hate doing dishes though');
+}
 
 
